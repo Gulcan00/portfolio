f5493a09788f8b970442419158c89b9d2306ae01
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-02-10 18:37:06 +0200

    Add multiple works to myWorks function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,44 @@ import Card from './card';
 import './style.css';
 
 function myWorks() {
-  const works = [{ title: 'Weather App' }];
+  const works = [
+    {
+      title: 'Weather App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo nunc, malesuada vitae congue a, lacinia id mi. Quisque tempor nibh libero, sed aliquet est sollicitudin vel. ',
+    },
+    {
+      title: 'Weather App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo nunc, malesuada vitae congue a, lacinia id mi. Quisque tempor nibh libero, sed aliquet est sollicitudin vel. ',
+    },
+    {
+      title: 'Weather App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo nunc, malesuada vitae congue a, lacinia id mi. Quisque tempor nibh libero, sed aliquet est sollicitudin vel. ',
+    },
+    {
+      title: 'Weather App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo nunc, malesuada vitae congue a, lacinia id mi. Quisque tempor nibh libero, sed aliquet est sollicitudin vel. ',
+    },
+    {
+      title: 'Weather App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo nunc, malesuada vitae congue a, lacinia id mi. Quisque tempor nibh libero, sed aliquet est sollicitudin vel. ',
+    },
+    {
+      title: 'Weather App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras justo nunc, malesuada vitae congue a, lacinia id mi. Quisque tempor nibh libero, sed aliquet est sollicitudin vel. ',
+    },
+  ];
   const workSection = document.querySelector('.my-work');
-
+  const cardsDiv = document.createElement('div');
   works.forEach((work) => {
-    workSection.appendChild(Card(work));
+    cardsDiv.appendChild(Card(work));
   });
+  workSection.appendChild(cardsDiv);
 }
 
 myWorks();
