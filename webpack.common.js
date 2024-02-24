acb9d8a4457cf893a23fe73f945373618c34c377
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-02-24 17:27:37 +0200

    Add placeholder image, fix dislaying image in html

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    // define HTML files here (the key is output file w/o ext)
+    index: './src/template.html', // => dist/index.html
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -10,19 +13,37 @@ module.exports = {
   },
   module: {
     rules: [
+      // Note: enable processing of HTML files from entry
+      {
+        test: /\.html$/,
+        loader: HtmlBundlerPlugin.loader, // HTML loader
+      },
+
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        // Note: this plugin extracts CSS self, no style-loader needed
+        use: ['css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          // Note: define output filename of images
+          filename: 'assets/img/[name].[hash:8][ext]',
+        },
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
+    new HtmlBundlerPlugin({
+      js: {
+        // output filename of extracted JS from source script loaded in HTML via `<script>` tag
+        filename: 'js/[name].[contenthash:8].js',
+      },
+      css: {
+        // output filename of extracted CSS from source style loaded in HTML via `<link>` tag
+        filename: 'css/[name].[contenthash:8].css',
+      },
     }),
   ],
 };
