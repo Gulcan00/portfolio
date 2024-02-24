9abbc54d17338c12792edd2216e3c787e371a95f
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-02-24 20:55:27 +0200

    Fix icons alignment mobile view

--- src/card.js
+++ src/card.js
@@ -1,3 +1,5 @@
+import './card.css';
+
 export default function Card({
   screenshotUrl,
   title,
@@ -15,22 +17,29 @@ export default function Card({
   cardContainer.appendChild(cardImg);
 
   const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
+  cardBody.classList.add('card__body');
+  const cardHeader = document.createElement('div');
+  cardHeader.classList.add('card__body__header');
   const titleH3 = document.createElement('h3');
   titleH3.classList.add('title');
   titleH3.textContent = title;
-  cardBody.appendChild(titleH3);
+  cardHeader.appendChild(titleH3);
 
+  const iconsDiv = document.createElement('div');
+  iconsDiv.classList.add('links');
   const repoIcon = document.createElement('a');
   repoIcon.innerHTML = '<i class="devicon-github-original colored"></i>';
   repoIcon.href = repoLink;
-  cardBody.appendChild(repoIcon);
+  iconsDiv.appendChild(repoIcon);
 
   const liveIcon = document.createElement('a');
   liveIcon.innerHTML =
     '<span class="material-symbols-outlined">open_in_new</span>';
   liveIcon.href = liveLink;
-  cardBody.appendChild(liveIcon);
+  iconsDiv.appendChild(liveIcon);
+
+  cardHeader.appendChild(iconsDiv);
+  cardBody.appendChild(cardHeader);
 
   const descriptionP = document.createElement('p');
   descriptionP.textContent = description;
